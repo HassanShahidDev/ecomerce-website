a33e35e52bec571aed0e336ae66181fa99ae40ac
HassanShahidDev <hassanshahid778866@gmail.com>
2025-10-19 12:06:34 -0700

    Fix imports and update frontend pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { ShopContext } from '../context/shopContext';
+import { ShopContext } from '../context/shopContext.jsx';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
